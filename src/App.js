e42f538345c44e5518eda6e29ad58b0fcaa2029c
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-09-15 17:26:45 -0400

    added the Github repo link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
+import React from "react";
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather Search Engine App</div>
+    <div className="App">
+        <h1>Weather Search Engine App</h1>
+        <footer>
+          This project was coded by {" "}
+          <a 
+              href="/" 
+              target="_blank" rel="noreferrer">
+              Rebecca Thombs
+              </a> and is open-sourced on {" "}
+          <a 
+              href="https://github.com/x0xBecka26x0x/search-engine-weather"
+              target="_blank" rel="noreferrer"
+          >Github</a>! 
+        </footer>
+      </div>
     );
 }
