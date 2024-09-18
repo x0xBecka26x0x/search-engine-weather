da80050dc977270723036f4956769acc73aa1b50
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-09-18 18:13:12 -0400

    api integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,70 +2,73 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
+
 export default function Weather(props) {
     const [weatherDetails, setWeatherDetails] = useState({ ready: false});
-    const [city, setCity] = useState(props.defaultCity);
+    const [city, setCity] = useState();
 
     function handleResponse(response) {
         setWeatherDetails({
-            ready: true, 
-            coordinates: response.data.coord,
-            temperature: response.data.main.temp, 
+            ready: true,
+            temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
-            date: new Date(response.data.dt * 1000), 
+            date: "Wednesday 07:00",
             description: response.data.weather[0].description,
-            icon: response.data.weather[0].icon, 
+            iconUrl: "https://ss1.gstatic.com/onebox/weather/64/partly_cloudy.png",
             wind: response.data.wind.speed,
-            city: response.data.name,
+            city: response.data.name
         });
     }
 
-        function handleSubmit(event) {
-            event.preventDefault();
-            searchForm();
-        }
-
-        function handleSearchBox(event) {
-            setCity(event.target.value);
-        }
-
-
-        function searchForm() {
-            const apiKey = "";
-            let apiUrl = ``;
-            axios.get(apiUrl).then(handleResponse);
-        }
-
-
-    if (weatherDetails.ready) {
-        return (
-            <div className="Weather">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-9">
-                        <input 
-                            type="search" 
-                            placeholder="Enter a city..." 
-                            className="control"
-                            autoFocus="on"
-                            onChange={handleSearchBox} 
-                        />
-                    </div>
-                    <div className="col-3">
-                        <input 
-                            type="submit" 
-                            value="Search" 
-                            className="btn btn primary w-100" 
-                        />
-                    </div>
+  if (weatherDetails.ready) {
+    return (
+      <div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+                    <input
+                        type="search"
+                        placeholder="Enter a city..."
+                        className="control"
+                    />
                 </div>
-            </form>
-            <WeatherInfo data={weatherDetails} />
-            <WeatherForecast coordinates={weatherDetails.coordinates} />
-        </div>    
-        );
-    } else {
-        searchForm();
-        return "Loading...";
-    }
+                <div className="col-3">
+                    <input type="submit" value="Search" className="btn btn-primary" />
+                </div>
+            </div>
+        </form>
+        <h1>{weatherDetails.city}</h1>
+        <ul>
+            <li>{weatherDetails.date}</li>
+            <li className="text-capitalize">{weatherDetails.description}</li>
+        </ul>
+        <div className="row mt-3">
+            <div className="col-6">
+              <img
+                    src="{weatherDetails.iconUrl}" 
+                    alt="{weatherDetails.description}"
+              />
+              <div className="float-left">
+                <span className="temperature">
+                    {Math.round(weatherDetails.temperature)}
+                </span>
+              <span className="unit">°C</span>
+              </div>
+            </div>
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Humidity: {weatherDetails.humidity}%</li>
+                    <li>Wind: {weatherDetails.wind}  km/h%</li>
+                </ul>
+            </div>
+        </div>
+    );
+} else {
+    const apiKey = "bf8f1010b3c486eaa378at4e5eo24f84";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+}
 }
